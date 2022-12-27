3f66b99bb30414bb3711c8490cb11dbdbc076eca
Becka McNally <beckamcnally@gmail.com>
2022-12-27 11:32:45 -0800

    base html and pseudo code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # quiz
+
+Pseudo Code
---
+++ assests/js/script.js
@@ -0,0 +1,25 @@
+
+/*
+1. User clicks start quiz
+    A.clock begins when quiz starts
+        i. event listener click start quiz
+2. have a question randomly chosen from array of questions
+    A. use random() to sort through array of questions
+        
+3. when question is generated display on screen 
+     A. use data ?? to hide and reveal questions 
+     B. Clock is also revealed
+
+4. user chooses answer by clicking on answer buttons or pressing corresponding letter key
+    A. use 
+
+5. wrong or correct if the displayed on screen under answers
+
+6. steps 2 - 5 repeat until 5 questions have been answered or time runs out 
+    A. create a for loop that runs five times
+
+7. once game is over display score (the amount of time left on the clock)
+
+8. give user option to go back (back to beginning) or enter their initials with their score 
+    A. if user enters initials take them to score board where they then have the option to go back or clear score board 
+*/
