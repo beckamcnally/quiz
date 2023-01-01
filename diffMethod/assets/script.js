715b43f9e03d521b92d4d667c1f91de43ebeb181
Becka McNally <beckamcnally@gmail.com>
2022-12-31 19:42:42 -0800

    fixed the indexing broke the answer checking

--- diffMethod/assets/script.js
+++ diffMethod/assets/script.js
@@ -66,7 +66,7 @@ var questionArray = [
         answer: "A. console.log()"
     }, // could not get my other questions because needed to use code snippets in question
 ]
-
+var currentQ = "";
 var questIndex = 0;
 var time = questionArray.length * 20;
 var endTime = "";
@@ -75,54 +75,51 @@ var userAns = "";
 // Function 1 - start the clock, hide intro and unhide the questions
 function beginGame(){
     intro.setAttribute("class", "hide");
-    
-
     var interval = setInterval(workingClock, 1000);
     endTime = interval
-
-
-        //ends game if clock gets to 0
-        getQuestions();
-    //else if there are no more questions capture time as score?
-    
- 
+    getQuestions();
 }
-
-
 
 // function to define clock
 function workingClock(){
     clock.removeAttribute("class", "hidden")
     time--;
     counter.textContent = time;
-    
-    if (time === 0 || questIndex > questionArray.length) {
-         //ends game if clock gets to 0
+    //ends game if clock gets to 0
+    if (time === 0) {  
          endGame()
-    } //else if there are no more questions capture time as score?
-
-    
+    }   
 }
 
+
+// Ends game and takes us to final score screen where user can enter their initials
 function endGame(){
         questContainer.setAttribute("class", "hide");
         gameOver.removeAttribute("class", "hide");
         clock.setAttribute("class", "hidden");
         clearInterval(endTime);  
         finalScore.textContent = time;
-        console.log(time)
+        console.log(time) 
         
+        // makes it so that someone can not get a negative score
+        if (time < 0)
+        finalScore.textContent = "0";
 }
+
 
 
 
 //Function 2 - get a question from the array, and the create the buttons for my array of choices
 function getQuestions() {
-    var currentQ = questionArray[questIndex];
+
+    // for (; questIndex < questionArray.length; questIndex++){ // when I add this line it console logs all index numbers but only display the last one. I tried using event.stopPropagation() and event.stopDefault() but said its not a function // when I do this I also have to takeout the if statement in the if answer statement 
+
+    var currentQ = questionArray[questIndex]; 
     question.textContent = currentQ.question;
     ansContainer.innerHTML = ""; // empty's the button field
-    
-// this for loop creates the button for each question
+    console.log("Index number " + questIndex); //when I first click start this is logging 0 however when I click the fist answer this is logging 1 when I click the second answer though it logs both two and three ?? It only lets you get three questions because of that
+
+    // this for loop creates the button for each question - it is inside the getQuestions function because I tried to make it its own function and their were too many variables that we undefined
     for (var i = 0; i < currentQ.choices.length; i++) {
         questContainer.removeAttribute("class", "hide");
         var selection = currentQ.choices[i];
@@ -131,21 +128,22 @@ function getQuestions() {
         selectBtn.setAttribute("value", selection);
         selectBtn.textContent = selection;
         ansContainer.appendChild(selectBtn);
-}   
-     
+   }
+return currentQ;
+}
+
+console.log(currentQ)
+// When I try to make this it's own function it tells me that currentQ is not defined I then add currentQ on the global scope at the top and if I do this by taking the line item var currentQ = questionArray[questIndex]; and moving it to the top then it says that it cna not rea properties of undefined in my if statement in my getAnswer(). If I add var currentQ; to the top same thing. So I add console.log(currentQ) after my getQuestions() and it is undefined. When I console.log(currentQ) at the end of getQuestions() I get what I want ( the question[0]). so I then do a return currentQ and put a console.log(currentQ) after my getQuestions() and it is again undefined --- I added "" to the var currentQ at the top and now it is working except that it is saying that all the answers are wrong.
 // listens to the answer buttons and determines if they are correct or not
 ansContainer.addEventListener("click",function getAnswer (event) {
     var clickedAns = event.target 
     userAns = clickedAns
-    finalScore.textContent = time;
-        console.log(time)  
     
-
-if (userAns.textContent === currentQ.answer){
+    if (userAns.textContent === currentQ.answer){
         rightWrong.textContent = "Correct!";
         ansContainer.innerHTML = "";
         question.innerHTML = "";
-        if (questIndex > questionArray.length){
+        if (questIndex >= questionArray.length){
             endGame()
         } else {
             showNextQuestion()
@@ -158,29 +156,23 @@ if (userAns.textContent === currentQ.answer){
         question.innerHTML = "";
         time = time -20;
         counter.textContent = time;
-        if (questIndex > questionArray.length){
+        if (questIndex >= questionArray.length){
             endGame()
         } else {
             showNextQuestion()
         }
-
       }
 });
-// function timeDeduction() {
-//     time - 20;
-//     counter.textContent = time;
-// }
+ 
 
+// this generates next question
 function showNextQuestion () {
-    questIndex++;
-    console.log(questIndex)
-    getQuestions()
-    console.log("next quest!!!")
+    questIndex++; // progresses the index by one so that it gens the next question
+    getQuestions() //?? maybe because I am calling the function with in the function    
+    console.log("that was question " + questIndex)
 }
 
-} 
 function leaderBoard (){
-    
     scoreBoard.removeAttribute("class", "hide");
     gameOver.setAttribute("class", "hide");
 
@@ -198,10 +190,7 @@ function leaderBoard (){
 function beginAgain(){
     location.reload()
 }
-    // if (i === -1){
-    //     finalScore.textContent = time;
-    //     endGame();
-    // };
+    
 again.addEventListener("click", beginGame)
 submitInt.addEventListener("click", leaderBoard)
 startBtn.addEventListener("click", beginGame)
